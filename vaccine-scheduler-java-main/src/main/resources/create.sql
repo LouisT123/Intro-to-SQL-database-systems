3de8521e55d7facc73aea7f72d0f45ad347b6570
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-03-21 13:19:43 -0700

    Update create.sql

--- vaccine-scheduler-java-main/src/main/resources/create.sql
+++ vaccine-scheduler-java-main/src/main/resources/create.sql
@@ -16,3 +16,18 @@ CREATE TABLE Vaccines (
     Doses int,
     PRIMARY KEY (Name)
 );
+
+CREATE TABLE Patients (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Appointment (
+	appointmentID int PRIMARY KEY,
+	caregiverName varchar(255) REFERENCES Caregivers(Username),
+	vaccineName varchar(255) REFERENCES Vaccines(Name),
+	patientName varchar(255) REFERENCES Patients(Username),
+	Time date
+);
